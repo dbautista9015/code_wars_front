d597fd23ec5df3729c8f9d28a11f9fe6b3c5e516
dylanmcfarlin <dylanmcfarlin10@gmail.com>
2022-03-22 13:40:43 -0700

    Login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Container, Row, Col, Nav, Navbar, NavDropdown, Button } from "react-bootstrap";
+import LoginPage from './Components/LoginPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter >
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+          <Navbar.Brand as={Link} to="/">Codewars Reservations</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/dashboardpage">Dashboard</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={Link} to="/adminpage">Admin</Nav.Link>
+              <Nav.Link eventKey={2} as={Link} to="/loginpage">Login</Nav.Link>
+              {/* <Nav.Link eventKey={2} as={Link} to="/signout">Login</Nav.Link> */}
+            </Nav>
+          </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path="/LoginPage" element={<LoginPage />} />
+          {/* <Route path="/DashboardPage" element={<DashboardPage />} />
+          <Route path="/AdminPage" element={<AdminPage />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
