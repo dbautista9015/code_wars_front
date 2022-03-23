84e22b2c1cc30654154ad51f40e283d195222f39
trentbarboza <tbarboza@codestack.co>
2022-03-23 14:16:34 -0700

    Started View All Users

--- src/AdminComponents/ViewAllUsersComponent.js
+++ src/AdminComponents/ViewAllUsersComponent.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import {
+  Accordion,
+  ListGroup,
+  Row,
+  Col,
+  Button,
+  Modal,
+  Form,
+} from "react-bootstrap";
 
 export default function ViewAllUsersComponent() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+        <Row>
+          <Col sm={3}>
+          <h3 className="headerText text-end" style={{ color: "white" }}>
+              Choose Cohort to View Users
+            </h3>
+          </Col>
+        </Row>
+      <Row className="justify-content-center" >
+        <Col sm={6}>
+          <Accordion >
+            <Accordion.Item eventKey="0" className="listGroupBG">
+              <Accordion.Header >Season 1 Cohort</Accordion.Header>
+              <Accordion.Body >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1" className="listGroupBG">
+              <Accordion.Header>Season 2 Cohort</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2" className="listGroupBG">
+              <Accordion.Header>Season 3 Cohort</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Col>
+      </Row>
+    </div>
+  );
 }
