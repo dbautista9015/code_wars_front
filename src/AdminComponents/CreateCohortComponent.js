4714bea069b6a6331f15eb486199e20de0a2aab3
DannyHT04 <dannyhat1@gmail.com>
2022-03-22 23:41:23 -0700

    added drop down for kata level in create cohort

--- src/AdminComponents/CreateCohortComponent.js
+++ src/AdminComponents/CreateCohortComponent.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import { Form, FloatingLabel } from 'react-bootstrap'
+import React from "react";
+import { Form, FloatingLabel } from "react-bootstrap";
 export default function CreateCohortComponent() {
-    return (
-        <>
-     <>
-  <FloatingLabel
-    controlId="floatingInput"
-    label="Email address"
-    className="mb-3"
-  >
-    <Form.Control className="loginForm loginFormText" type="email" placeholder="name@example.com" />
-  </FloatingLabel>
-  <FloatingLabel controlId="floatingPassword" label="Password">
-    <Form.Control className="loginForm loginFormText" type="password" placeholder="Password" />
-  </FloatingLabel>
-</>
-
-        </>
-    )
+  return (
+    <>
+      <h3 className="headerText" style={{ color: "white" }}>
+        Create Cohort
+      </h3>
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Enter Cohort's Name"
+        className="mb-3 allText"
+      >
+        <Form.Control type="text" placeholder="Cohort's Name" />
+      </FloatingLabel>
+      <Form.Select aria-label="Default select example">
+        <option>Choose Kata's Level</option>
+        <option value="8">8 kata</option>
+        <option value="7">7 kata</option>
+        <option value="6">6 kata</option>
+        <option value="5">5 kata</option>
+        <option value="4">4 kata</option>
+        <option value="3">3 kata</option>
+        <option value="2">2 kata</option>
+        <option value="1">1 kata</option>
+      </Form.Select>
+    </>
+  );
 }
