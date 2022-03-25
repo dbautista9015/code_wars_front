ea4e6291ceebc71e1b2a06cc390184ec09640b04
sophieespejo <sophmeas@gmail.com>
2022-03-25 13:51:20 -0700

    moved stuff in dashboard

--- src/Components/DashboardPage.js
+++ src/Components/DashboardPage.js
@@ -24,13 +24,12 @@ export default function DashboardPage() {
       if (storedCodewarsName != null) {
         setCodewarsName(storedCodewarsName);
         setCohortName(cohortName);
+        let cohortInfo = await GetCohortByCohortName(cohortName);
+        setCohortInfo(cohortInfo);
+        let completed = await DoesUserExist(storedCodewarsName);
+        let totalCompleted = completed.codeChallenges.totalCompleted;
+        setTotalCompleted(totalCompleted);
       }
-      let cohortInfo = await GetCohortByCohortName(cohortName);
-      setCohortInfo(cohortInfo);
-      let completed = await DoesUserExist(storedCodewarsName);
-      let totalCompleted = completed.codeChallenges.totalCompleted;
-      setTotalCompleted(totalCompleted);
-      console.log(totalCompleted)
     }
   }, []);
 
@@ -78,7 +77,6 @@ export default function DashboardPage() {
                   <YourPastKatasComponent />
                 </Tab.Pane>
                 <Tab.Pane eventKey="fourth">
-                  {/* <ViewAllUsersComponent /> */}
                 </Tab.Pane>
               </Tab.Content>
             </Col>
@@ -88,59 +86,3 @@ export default function DashboardPage() {
     </>
   );
 }
-// {
-//     "id": "534e01fbbb17187c7e0000c6",
-//     "name": "Make a spiral",
-//     "slug": "make-a-spiral",
-//     "category": "algorithms",
-//     "publishedAt": "2014-04-16T09:19:39.444Z",
-//     "approvedAt": "2014-06-11T19:56:13.812Z",
-//     "languages": [
-//         "coffeescript",
-//         "javascript",
-//         "python",
-//         "ruby",
-//         "haskell",
-//         "swift",
-//         "java",
-//         "julia",
-//         "rust",
-//         "csharp",
-//         "cobol",
-//         "c"
-//     ],
-//     "url": "https://www.codewars.com/kata/534e01fbbb17187c7e0000c6",
-//     "rank": {
-//         "id": -3,
-//         "name": "3 kyu",
-//         "color": "blue"
-//     },
-//     "createdAt": "2014-04-16T04:07:23.255Z",
-//     "createdBy": {
-//         "username": "Bugari",
-//         "url": "https://www.codewars.com/users/Bugari"
-//     },
-//     "approvedBy": {
-//         "username": "jhoffner",
-//         "url": "https://www.codewars.com/users/jhoffner"
-//     },
-//     "description": "Your task, is to create a NxN spiral with a given `size`.  For example, spiral with size 5 should look like this:  ``` 00000 ....0 000.0 0...0 00000 ```  and with the size 10:  ``` 0000000000 .........0 00000000.0 0......0.0 0.0000.0.0 0.0..0.0.0 0.0....0.0 0.000000.0 0........0 0000000000 ```  Return value should contain array of arrays, of `0` and `1`, with the first row being composed of `1`s. For example for given size `5` result should be:   ```javascript [[1,1,1,1,1],[0,0,0,0,1],[1,1,1,0,1],[1,0,0,0,1],[1,1,1,1,1]] ``` ```rust [[1,1,1,1,1],[0,0,0,0,1],[1,1,1,0,1],[1,0,0,0,1],[1,1,1,1,1]] ``` ```julia [1 1 1 1 1; 0 0 0 0 1; 1 1 1 0 1; 1 0 0 0 1; 1 1 1 1 1] ```  Because of the edge-cases for tiny spirals, the size will be at least 5.  General rule-of-a-thumb is, that the snake made with '1' cannot touch to itself. ",
-//     "totalAttempts": 65457,
-//     "totalCompleted": 8115,
-//     "totalStars": 1074,
-//     "voteScore": 1008,
-//     "tags": [
-//         "Algorithms",
-//         "Logic",
-//         "Arrays",
-//         "Data Types",
-//         "Control Flow",
-//         "Basic Language Features",
-//         "Fundamentals"
-//     ],
-//     "contributorsWanted": true,
-//     "unresolved": {
-//         "issues": 1,
-//         "suggestions": 2
-//     }
-// }
